e8e348d9071558370a59d734be6b0485284fd9dc
kevinmaiden7 <kevin.m.gallego@gmail.com>
2021-01-24 18:18:28 -0500

    Uploading files

---
+++ hello.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+import "math"
+import "rsc.io/quote"
+
+func main(){
+	fmt.Println("Greetings from Go!")
+	fmt.Println(quote.Hello())
+	fmt.Println(quote.Go())
+	fmt.Println(quote.Opt())
+	fmt.Println("---")
+
+	const base = 2
+	fmt.Println("Base:", base)
+	for i := 0; i < 13; i++	{
+		fmt.Printf("%.1f", math.Pow(float64(base), float64(i)))
+		fmt.Println("")
+	}
+}
